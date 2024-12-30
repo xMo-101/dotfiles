9fe51f3262ed507ca984ef49c5fcf5406e3c1d99
xMo <159272311+xMo-101@users.noreply.github.com>
2024-12-30 15:17:13 +0100

    Update autostart.sh

--- hypr/.config/hypr/autostart.sh
+++ hypr/.config/hypr/autostart.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # hyprpaper
+if pgrep -x "hyprpaper" > /dev/null
+then
+	pkill hyprpaper
+fi
 hyprpaper &
 # waybar:
 if pgrep -x "waybar" > /dev/null
@@ -15,3 +19,15 @@ then
     pkill udiskie
 fi
 udiskie -t &
+# nm-applet:
+if pgrep -x "nm-applet" > /dev/null
+then
+    pkill nm-applet
+fi
+nm-applet &
+# nwg-dock-hyprland:
+if pgrep -x "nwg-dock-hyprla" > /dev/null
+then
+    pkill nwg-dock-hyprla
+fi
+nwg-dock-hyprland -d -f -lp 'end' -i 36 &
