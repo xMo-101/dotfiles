3221b3284df624afc3496c976fc8da22cead025c
xMo-101 <awan.moeez52@gmail.com>
2025-08-06 14:16:40 +0200

    centralized imports

---
+++ ags/.config/ags/utils/imports.ts
@@ -0,0 +1,10 @@
+// THIS FILE EXISTS TO MAKE HANDLING IMPORT PATHS EASIER
+export { For, With, createState, createBinding } from "ags";
+export { Astal } from "ags/gtk4";
+export { app } from "ags/gtk4/app";
+
+// gi://
+import Mpris from "gi://AstalMpris?version=0.1";
+import Gtk from "gi://Gtk?version=4.0";
+import GLib from "gi://GLib";
+export { Mpris, Gtk, GLib };
