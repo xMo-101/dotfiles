12976484f23cfab765ee63c98edc3605d1729ce6
xMo-101 <awan.moeez52@gmail.com>
2025-08-06 19:42:13 +0200

    saving

--- ags/.config/ags/widget/topbar/notch/modules/Opts.tsx
+++ ags/.config/ags/widget/topbar/notch/modules/Opts.tsx
@@ -1,46 +1,74 @@
+import { Gtk } from "@/utils/imports";
 import GLib from "gi://GLib";
-import Gtk from "gi://Gtk?version=4.0";
+
+function ButtonTemplate({
+  icon,
+  action,
+  tooltip_text = "",
+}: {
+  icon: String;
+  action: () => void;
+  tooltip_text: String;
+}) {
+  return (
+    <button class="button-long" onClicked={action} tooltip_text={tooltip_text}>
+      <image iconName={icon} pixelSize={24} />
+    </button>
+  );
+}
+
+function home(): String {
+  return GLib.get_home_dir();
+}
+
+function scripts(): String {
+  return home() + "/.config/hypr/scripts";
+}
+
+function ScreenshotAreaButton() {
+  return (
+    <ButtonTemplate
+      icon="accessories-screenshot-symbolic"
+      action={() =>
+        GLib.spawn_command_line_async(`${scripts()}/hyprshot/hyprshot-area.sh`)
+      }
+      tooltip_text="screenshot area"
+    />
+  );
+}
+
+function ScreenshotMonitorButton() {
+  return (
+    <ButtonTemplate
+      icon="accessories-screenshot"
+      action={() =>
+        GLib.spawn_command_line_async(
+          `${scripts()}/hyprshot/hyprshot-monitor.sh`,
+        )
+      }
+      tooltip_text="screenshot monitor"
+    />
+  );
+}
+
+function ColorpickerButton() {
+  return (
+    <ButtonTemplate
+      icon="color-select-symbolic"
+      action={() =>
+        GLib.spawn_command_line_async(`${home()}/.config/hypr/hyprpicker.sh`)
+      }
+    />
+  );
+}
 
 export function Opts() {
   return (
     <box $type="named" name="page-opts">
-      <Gtk.FlowBox>
-        <button
-          label="screenie"
-          onClicked={() =>
-            GLib.spawn_command_line_async("~/.config/hypr/hyprshot.sh")
-          }
-        />
-        <button
-          label="screenie"
-          onClicked={() =>
-            GLib.spawn_command_line_async("~/.config/hypr/hyprshot.sh")
-          }
-        />
-        <button
-          label="screenie"
-          onClicked={() =>
-            GLib.spawn_command_line_async("~/.config/hypr/hyprshot.sh")
-          }
-        />
-        <button
-          label="screenie"
-          onClicked={() =>
-            GLib.spawn_command_line_async("~/.config/hypr/hyprshot.sh")
-          }
-        />
-        <button
-          label="screenie"
-          onClicked={() =>
-            GLib.spawn_command_line_async("~/.config/hypr/hyprshot.sh")
-          }
-        />
-        <button
-          label="screenie"
-          onClicked={() =>
-            GLib.spawn_command_line_async("~/.config/hypr/hyprshot.sh")
-          }
-        />
+      <Gtk.FlowBox maxChildrenPerLine={4} minChildrenPerLine={1}>
+        <ScreenshotAreaButton />
+        <ScreenshotMonitorButton />
+        <ColorpickerButton />
       </Gtk.FlowBox>
     </box>
   );
