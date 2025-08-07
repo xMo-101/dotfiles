c8208200c5030326ada7c53e9defa3e14bfcd854
xMo-101 <awan.moeez52@gmail.com>
2025-08-07 20:13:15 +0200

    saving

--- ags/.config/ags/widget/topbar/notch/modules/Opts.tsx
+++ ags/.config/ags/widget/topbar/notch/modules/Opts.tsx
@@ -1,5 +1,4 @@
-import { Gtk } from "@/utils/imports";
-import GLib from "gi://GLib";
+import { Gtk, GLib, createState } from "@/utils/imports";
 
 function ButtonTemplate({
   icon,
@@ -11,8 +10,43 @@ function ButtonTemplate({
   tooltip_text: String;
 }) {
   return (
-    <button class="button-long" onClicked={action} tooltip_text={tooltip_text}>
-      <image iconName={icon} pixelSize={24} />
+    <button class="button-large" onClicked={action} tooltip_text={tooltip_text}>
+      <image iconName={icon} pixelSize={32} />
+    </button>
+  );
+}
+
+function ToggleButtonTemplate({
+  icon,
+  action,
+  tooltip_text,
+  init,
+}: {
+  icon: string;
+  action: () => void;
+  tooltip_text: string;
+  init: boolean;
+}) {
+  const [classState, setClassState] = createState(
+    init ? "button-toggled" : "button-large",
+  );
+
+  const toggleButton = () => {
+    setClassState((prev) =>
+      prev === "button-toggled" ? "button-large" : "button-toggled",
+    );
+  };
+
+  return (
+    <button
+      class={classState}
+      onClicked={() => {
+        action();
+        toggleButton();
+      }}
+      tooltip_text={tooltip_text}
+    >
+      <image iconName={icon} pixelSize={32} />
     </button>
   );
 }
@@ -26,49 +60,122 @@ function scripts(): String {
 }
 
 function ScreenshotAreaButton() {
+  const action = () =>
+    GLib.spawn_command_line_async(`${scripts()}/hyprshot/hyprshot-area.sh`);
+
   return (
     <ButtonTemplate
       icon="accessories-screenshot-symbolic"
-      action={() =>
-        GLib.spawn_command_line_async(`${scripts()}/hyprshot/hyprshot-area.sh`)
-      }
+      action={action}
       tooltip_text="screenshot area"
     />
   );
 }
 
 function ScreenshotMonitorButton() {
+  const action = () =>
+    GLib.spawn_command_line_async(`${scripts()}/hyprshot/hyprshot-monitor.sh`);
+
   return (
     <ButtonTemplate
       icon="accessories-screenshot"
-      action={() =>
-        GLib.spawn_command_line_async(
-          `${scripts()}/hyprshot/hyprshot-monitor.sh`,
-        )
-      }
+      action={action}
       tooltip_text="screenshot monitor"
     />
   );
 }
 
 function ColorpickerButton() {
+  const action = () =>
+    GLib.spawn_command_line_async(`${scripts()}/hyprpicker/hyprpicker.sh`);
   return (
     <ButtonTemplate
       icon="color-select-symbolic"
-      action={() =>
-        GLib.spawn_command_line_async(`${home()}/.config/hypr/hyprpicker.sh`)
-      }
+      action={action}
+      tooltip_text="colorpicker"
+    />
+  );
+}
+
+function HyprlandReloadButton() {
+  const action = () =>
+    GLib.spawn_command_line_async(`${scripts()}/utils/hyprreload.sh`);
+  return <ButtonTemplate icon="view-refresh-symbolic" action={action} />;
+}
+
+function DNDButton() {
+  const action = () => print("WIP");
+  return (
+    <ToggleButtonTemplate
+      init={false}
+      action={action}
+      icon="notifications-disabled-symbolic"
+      tooltip_text="DND"
+    />
+  );
+}
+
+function SpeedtestButton() {
+  const action = () =>
+    GLib.spawn_command_line_async(`alacritty -e bash -c 'speedtest';read`);
+  return (
+    <ButtonTemplate
+      icon="network-transmit-receive-symbolic"
+      action={action}
+      tooltip_text="network speedtest"
+    />
+  );
+}
+
+function RecordScreenButton() {
+  const action = () =>
+    GLib.spawn_command_line_async(`${scripts()}/wf-recorder/wf-recorder.sh`);
+  return (
+    <ToggleButtonTemplate
+      action={action}
+      icon="media-record-symbolic"
+      init={false}
+      tooltip_text="toggle screen recording"
+    />
+  );
+}
+
+function SunsetButton() {
+  const action = () => print("WIP");
+  return (
+    <ToggleButtonTemplate
+      action={action}
+      icon="weather-clear-night-symbolic"
+      init={false}
+      tooltip_text="bluelight filter"
     />
   );
 }
 
 export function Opts() {
+  const childrenPerLine = 5;
   return (
-    <box $type="named" name="page-opts">
-      <Gtk.FlowBox maxChildrenPerLine={4} minChildrenPerLine={1}>
+    <box $type="named" name="page-opts" orientation={Gtk.Orientation.VERTICAL}>
+      <Gtk.FlowBox
+        homogeneous={true}
+        maxChildrenPerLine={childrenPerLine}
+        minChildrenPerLine={1}
+      >
         <ScreenshotAreaButton />
         <ScreenshotMonitorButton />
         <ColorpickerButton />
+        <HyprlandReloadButton />
+        <SpeedtestButton />
+      </Gtk.FlowBox>
+      <Gtk.Separator />
+      <Gtk.FlowBox
+        homogeneous={true}
+        maxChildrenPerLine={childrenPerLine}
+        minChildrenPerLine={1}
+      >
+        <DNDButton />
+        <RecordScreenButton />
+        <SunsetButton />
       </Gtk.FlowBox>
     </box>
   );
