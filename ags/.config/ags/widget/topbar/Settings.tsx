a0a3ca29c95ff97994d8474a29f92de349a6fc19
xMo-101 <awan.moeez52@gmail.com>
2025-08-07 16:14:59 +0200

    Popover instead of window

--- ags/.config/ags/widget/topbar/Settings.tsx
+++ ags/.config/ags/widget/topbar/Settings.tsx
@@ -1,28 +1,12 @@
-import app from "ags/gtk4/app";
-import { Astal, Gtk } from "ags/gtk4";
-import { createState, createBinding } from "ags";
+import { Gtk, createBinding } from "@/utils/imports";
+import { Brightness } from "@/utils/brightness";
 import Wp from "gi://AstalWp";
 
-import { Cpu } from "./modules/cpu";
-import { Memory } from "./modules/memory";
-import { myBattery } from "./modules/battery";
-import { Brightness } from "../../utils/brightness";
-
-const [isSettingsVisible, setSettingsVisibility] = createState(false);
-let vv = false;
-function toggleSettingsVisibility() {
-  vv = !vv;
-  setSettingsVisibility(vv);
-}
-
-const brightness = Brightness.get_default();
-const WINDOW_NAME = "settings-window";
-
-export function SettingsWindow() {
-  const wp = Wp.get_default()!;
-  const speaker = wp.audio.get_default_speaker()!;
-  const mic = wp.audio.get_default_microphone()!;
-
+export function Settings() {
+  const wp = Wp.get_default();
+  const speaker = wp?.get_default_speaker();
+  const mic = wp?.get_default_microphone();
+  const brightness = Brightness.create();
   const audioVolText = createBinding(
     speaker,
     "volume",
@@ -41,174 +25,102 @@ export function SettingsWindow() {
         .toString()
         .padStart(3, " ")}%`,
   );
-  const brightnessText = createBinding(
-    brightness,
-    "screen",
-  )(
-    (v) =>
-      `${Math.round(v * 100)
-        .toString()
-        .padStart(3, " ")}%`,
-  );
 
   return (
-    <window
-      name={WINDOW_NAME}
-      namespace={WINDOW_NAME}
-      $={(self) => app.add_window(self)}
-      anchor={Astal.WindowAnchor.TOP | Astal.WindowAnchor.RIGHT}
-      layer={Astal.Layer.OVERLAY}
-      exclusivity={Astal.Exclusivity.NORMAL}
-      visible={isSettingsVisible}
-      marginTop={5}
-      marginRight={5}
-      keymode={Astal.Keymode.ON_DEMAND}
-      class="outermost"
-    >
-      <box
-        hexpand={true}
-        vexpand={true}
-        valign={Gtk.Align.FILL}
-        widthRequest={325}
-        $={(self) => {
-          const click = new Gtk.GestureClick();
-          click.connect("pressed", () => togglesettingsVisibility());
-          self.add_controller(click);
-        }}
-        class="padded_container"
-      >
-        <box orientation={Gtk.Orientation.VERTICAL}>
-          {/* TOP: Battery, CPU, RAM */}
+    <menubutton class="button-long">
+      <image iconName="preferences-system-symbolic"></image>
+      <Gtk.Popover widthRequest={400}>
+        <box spacing={4} orientation={Gtk.Orientation.VERTICAL}>
           <box
             orientation={Gtk.Orientation.HORIZONTAL}
             hexpand
-            valign={Gtk.Align.FILL}
+            valign={Gtk.Align.CENTER}
           >
-            {myBattery() ?? null}
-            {Cpu()}
-            {Memory()}
+            <Gtk.GestureClick
+              onPressed={() => speaker?.set_mute(!speaker.get_mute())}
+            />
+            <slider
+              class="slider with-icon"
+              orientation={Gtk.Orientation.HORIZONTAL}
+              value={createBinding(speaker, "volume")}
+              min={0}
+              max={1}
+              step={0.1}
+              onChangeValue={({ value }) => speaker.set_volume(value)}
+              hexpand
+            />
+            <image
+              iconName={createBinding(speaker, "volume_icon")}
+              halign={Gtk.Align.START}
+              valign={Gtk.Align.CENTER}
+              class="slider-icon"
+            />
+            <label
+              label={audioVolText}
+              valign={Gtk.Align.CENTER}
+              class="volume-text"
+            />
+          </box>
+          <box
+            orientation={Gtk.Orientation.HORIZONTAL}
+            hexpand
+            valign={Gtk.Align.CENTER}
+          >
+            <Gtk.GestureClick
+              onPressed={() => mic?.set_mute(!mic.get_mute())}
+            />
+            <slider
+              class="slider with-icon"
+              orientation={Gtk.Orientation.HORIZONTAL}
+              value={createBinding(mic, "volume")}
+              min={0}
+              max={1}
+              step={0.1}
+              onChangeValue={({ value }) => mic.set_volume(value)}
+              hexpand
+            />
+            <image
+              iconName={createBinding(mic, "volume_icon")}
+              halign={Gtk.Align.START}
+              valign={Gtk.Align.CENTER}
+              class="slider-icon"
+            />
+            <label
+              label={micVolText}
+              valign={Gtk.Align.CENTER}
+              class="volume-text"
+            />
           </box>
 
-          {/* Separator between status and sliders */}
-          <Gtk.Separator orientation={Gtk.Orientation.HORIZONTAL} />
-
-          {/* BOTTOM: Sliders each in their own row */}
-          <box orientation={Gtk.Orientation.VERTICAL}>
-            {/* Speaker Row */}
-            <box
-              $={(self) => {
-                const click = new Gtk.GestureClick();
-                click.connect("pressed", () => (speaker.mute = !speaker.mute));
-                self.add_controller(click);
-              }}
-            >
-              <box
-                orientation={Gtk.Orientation.HORIZONTAL}
-                hexpand
-                valign={Gtk.Align.CENTER}
-              >
-                <slider
-                  class="slider with-icon"
-                  orientation={Gtk.Orientation.HORIZONTAL}
-                  value={createBinding(speaker, "volume")}
-                  min={0}
-                  max={1}
-                  step={0.01}
-                  onChangeValue={({ value }) => speaker.set_volume(value)}
-                  hexpand
-                />
-                <image
-                  iconName={createBinding(speaker, "volume_icon")}
-                  halign={Gtk.Align.START}
-                  valign={Gtk.Align.CENTER}
-                  class="slider-icon"
-                />
-                <label
-                  label={audioVolText}
-                  valign={Gtk.Align.CENTER}
-                  class="volume-text"
-                />
-              </box>
-            </box>
-
-            {/* Mic Row */}
-            <box
-              $={(self) => {
-                const click = new Gtk.GestureClick();
-                click.connect("pressed", () => (speaker.mute = !speaker.mute));
-                self.add_controller(click);
-              }}
-            >
-              <box
-                orientation={Gtk.Orientation.HORIZONTAL}
-                hexpand
-                valign={Gtk.Align.CENTER}
-              >
-                <slider
-                  class="slider with-icon"
-                  orientation={Gtk.Orientation.HORIZONTAL}
-                  value={createBinding(mic, "volume")}
-                  min={0}
-                  max={1}
-                  step={0.01}
-                  onChangeValue={({ value }) => mic.set_volume(value)}
-                  hexpand
-                />
-                <image
-                  iconName={createBinding(mic, "volume_icon")}
-                  halign={Gtk.Align.START}
-                  valign={Gtk.Align.CENTER}
-                  class="slider-icon"
-                />
-                <label
-                  label={micVolText}
-                  valign={Gtk.Align.CENTER}
-                  class="volume-text"
-                />
-              </box>
-            </box>
-
-            {/* Brightness Row */}
-            <box>
-              <box
-                orientation={Gtk.Orientation.HORIZONTAL}
-                hexpand
-                valign={Gtk.Align.CENTER}
-              >
-                <slider
-                  class="slider with-icon"
-                  orientation={Gtk.Orientation.HORIZONTAL}
-                  value={createBinding(brightness, "screen")}
-                  min={0}
-                  max={1}
-                  step={0.01}
-                  onChangeValue={({ value }) => (brightness.screen = value)}
-                  hexpand
-                />
-                <image
-                  iconName="display-brightness-symbolic"
-                  halign={Gtk.Align.START}
-                  valign={Gtk.Align.CENTER}
-                  class="slider-icon"
-                />
-                <label
-                  label={brightnessText}
-                  valign={Gtk.Align.CENTER}
-                  class="volume-text"
-                />
-              </box>
-            </box>
+          <box
+            orientation={Gtk.Orientation.HORIZONTAL}
+            hexpand
+            valign={Gtk.Align.CENTER}
+          >
+            <slider
+              class="slider with-icon"
+              orientation={Gtk.Orientation.HORIZONTAL}
+              min={0}
+              max={1}
+              step={0.1}
+              value={brightness.value}
+              onValueChanged={({ value }) => brightness.setBrightness(value)}
+              hexpand
+            />
+            <image
+              iconName="display-brightness-symbolic"
+              halign={Gtk.Align.START}
+              valign={Gtk.Align.CENTER}
+              class="slider-icon"
+            />
+            <label
+              label={brightness.text}
+              valign={Gtk.Align.CENTER}
+              class="volume-text"
+            />
           </box>
         </box>
-      </box>
-    </window>
-  );
-}
-
-export function Settings() {
-  return (
-    <button class="button-long" onClicked={toggleSettingsVisibility}>
-      <image iconName="preferences-system-symbolic"></image>
-    </button>
+      </Gtk.Popover>
+    </menubutton>
   );
 }
